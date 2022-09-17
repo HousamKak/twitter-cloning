55f9b9e291225dc60d14bbbbaf05bd1e02666488
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-09-17 18:56:22 +0300

    editting signup attributes

---
+++ Backend/php-apis/signup.php
@@ -0,0 +1,20 @@
+<?php
+
+include("connection.php");
+
+$full_name = $_POST["full_name"];
+$user_name = $_POST["user_name"];
+$email = $_POST["email"];
+$dob = $_POST["dob"];
+$user_password = $_POST["user_password"];
+
+$query = $mysqli -> prepare("INSERT INTO users (full_name, user_name, email, dob, user_password) VALUE (?, ?, ?, ?, ?)");
+$query -> bind_param("sssss", $full_name, $user_name, $email, $dob, $user_password);
+$query -> execute();
+
+$response = [];
+$response["success"] = true;
+
+echo json_encode($response);
+
+?>
