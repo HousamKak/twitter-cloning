28b51fee9b5bdb170f7e7a664117803469c3900d
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-09-17 16:23:52 +0300

    testing posting data

--- Backend/signup.php
+++ Backend/signup.php
@@ -2,14 +2,14 @@
 
 include("connection.php");
 
-$full_name = $_POST["full_name"];
-$dob = $_POST["dob"];
-$email = $_POST["email"];
+// $full_name = $_POST["full_name"];
+// $dob = $_POST["dob"];
+// $email = $_POST["email"];
 $user_name = $_POST["user_name"];
 $user_password = $_POST["user_password"];
 
-$query = $mysqli -> prepare("INSERT INTO users (full_name, dob, email, user_name, user_password) VALUE (?, ?, ?, ?, ?)");
-$query -> bind_param("sssss", $full_name, $dob, $email, $user_name, $user_password);
+$query = $mysqli -> prepare("INSERT INTO users (user_name, user_password) VALUE (?, ?)");
+$query -> bind_param("ss", $user_name, $user_password);
 $query -> execute();
 
 $response = [];
