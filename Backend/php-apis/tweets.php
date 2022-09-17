dd8ebb2f9281cfb66747eaf8e95f53be507ddec1
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-09-17 23:21:19 +0300

    modified tweets query

--- Backend/php-apis/tweets.php
+++ Backend/php-apis/tweets.php
@@ -2,17 +2,16 @@
 
 include("connection.php");
 
+$user_id = $_POST["user_id"];
 $tweet_text = $_POST["tweet_text"];
 $tweet_picture = $_POST["tweet_picture"];
 
 
-$query = $mysqli -> prepare("INSERT INTO tweets (tweet_text, tweet_picture) VALUE (?, ?)");
-$query -> bind_param("ss", $tweet_text, $tweet_picture);
+$query = $mysqli -> prepare("INSERT INTO tweets (user_id ,tweet_text, tweet_picture) VALUE (?, ?,?)");
+$query -> bind_param("sss", $user_id,$tweet_text, $tweet_picture);
 $query -> execute();
 
 $response = [];
 $response["success"] = true;
 
 echo json_encode($response);
-
-?>
