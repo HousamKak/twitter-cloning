937c41dd25990c6a1c4aebe29b01833e51d0169a
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-09-17 18:59:43 +0300

    editting login attributes

---
+++ Backend/php-apis/login.php
@@ -0,0 +1,18 @@
+<?php
+
+include("connection.php");
+
+$query = $mysqli -> prepare("SELECT user_id, user_name, user_password FROM users");
+$query -> execute();
+$array = $query->get_result();
+
+$response = [];
+
+while($a = $array->fetch_assoc()){
+    $response[] = $a;
+}
+
+$json = json_encode($response);
+echo $json;
+
+?>
