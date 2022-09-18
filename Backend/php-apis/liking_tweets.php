0174c82aa7bdc4f814b3f87927a3d56095ccb4b8
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-09-18 10:58:24 +0300

    deleted files from before

--- Backend/php-apis/liking_tweets.php
+++ Backend/php-apis/liking_tweets.php
@@ -6,13 +6,12 @@ $user_id = $_POST["user_id"];
 $tweet_id = $_POST["tweet_id"];
 
 
-$query = $mysqli -> prepare("INSERT INTO user_likes (user_id, tweet_id) VALUE (?, ?)");
-$query -> bind_param("ii", $user_id, $tweet_id);
-$query -> execute();
+$query = $mysqli->prepare("INSERT INTO user_likes(user_id, tweet_id) VALUE (?, ?)");
+$query->bind_param("ii", $user_id, $tweet_id);
+$query->execute();
 
 $response = [];
 $response["success"] = true;
 
 echo json_encode($response);
-
 ?>
