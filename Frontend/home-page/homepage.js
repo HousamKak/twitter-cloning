209fcb364f272f38f790e8f2c6a4efce16e47e32
“Housam <housam.kak20@gmail.com>
2022-09-18 23:17:39 +0300

    still working on the element js

--- Frontend/home-page/homepage.js
+++ Frontend/home-page/homepage.js
@@ -1,19 +1,23 @@
 // logged_in_user = localStorage.getItem("active-user")
 //   ? JSON.parse(localStorage.getItem("active-user"))
 //   : "";
+user_name = "Housam Kak";
+profile_photo = "/Frontend/home-page/Images/demo-img.jpg";
+likes_number = "4";
+mystatus = document.querySelector("#status");
 tweet = `<div class="reusable-element">
             <div class="title-content">
               <div class="image-content">
                 <img
                   class="profile-photo"
-                  src="${profile-photo}"
+                  src="${profile_photo}"
                 />
                 <h1>${user_name}</h1>
               </div>
               <p style="color: white">...</p>
             </div>
             <div class="main-text">
-              <p>${tweet-text}
+              <p>${mystatus.textContent}
               </p>
             </div>
             <div class="likes">
@@ -24,3 +28,10 @@ tweet = `<div class="reusable-element">
               <p id="likes-number">${likes_number}</p>
             </div>
           </div>`;
+
+tweet_btn_small = document.querySelector("#tweet-btn-small");
+
+tweet_btn_small.addEventListener("click", () => {
+
+    
+});
