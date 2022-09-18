880f6078b3fe59b0633ab647010855caafb43145
“Housam <housam.kak20@gmail.com>
2022-09-18 14:58:55 +0300

    Initially written at mhmd laptop copied the file into my laptop to commit my work

--- Backend/php-apis/login.php
+++ Backend/php-apis/login.php
@@ -2,16 +2,40 @@
 
 include("connection.php");
 
-$query = $mysqli -> prepare("SELECT user_id, user_name, user_password FROM users");
-$query -> execute();
+//getting the data from the frontend 
+$user_name = $_POST["user_name"];
+
+// hashing the password
+$user_password = hash("sha256", $_POST["user_password"]);
+$user_password .= "a";
+
+// getting the query result
+$query = $mysqli->prepare("SELECT user_name, user_password FROM users where user_name=?");
+$query->bind_param("s", $user_name);
+$query->execute();
 $array = $query->get_result();
 
+// Constructing emoty array
 $response = [];
 
-while($a = $array->fetch_assoc()){
-    $response[] = $a;
+// Check initially if the user exists
+if ($array->num_rows == 1) {
+    $response["ispresent"] = true;
+
+    // fetching the data base password 
+    $data_base_pass = $array->fetch_assoc()['user_password'];
+
+    //  Check for the validity of input password
+    if ($data_base_pass != $user_password) {
+        $response["pass_valid"] = false;
+    } else {
+        $response["pass_valid"] = true;
+    }
+} else {
+    $response["ispresent"] = false;
 }
 
+// Sending the json object
 $json = json_encode($response);
 echo $json;
 
