f7fade3aee467f69de0511a30ae1a88a39d63ffd
“Housam <housam.kak20@gmail.com>
2022-09-18 23:07:30 +0300

    Added heart svg constructed the heart element

--- Frontend/home-page/homepage.js
+++ Frontend/home-page/homepage.js
@@ -1,27 +1,26 @@
 // logged_in_user = localStorage.getItem("active-user")
 //   ? JSON.parse(localStorage.getItem("active-user"))
 //   : "";
-tweet=
-<div class="happening">
-  <h1>Home</h1>
-  <div class="im-happen">
-    <img class="profile-photo" src="/Frontend/home-page/Images/demo-img.jpg" />
-    <input placeholder="What's happening?" />
-  </div>
-  <div class="tweet-btn">
-    <button>Tweet</button>
-  </div>
-</div>;
-
-
-let ahmad = `<div class="happening">
-<h1>Home</h1>
-<div class="im-happen">
-  <img class="profile-photo" src="${}" />
-  <input placeholder="What's happening?" />
-</div>
-<div class="tweet-btn">
-  <button>Tweet</button>
-</div>
-</div>`
-
+tweet = `<div class="reusable-element">
+            <div class="title-content">
+              <div class="image-content">
+                <img
+                  class="profile-photo"
+                  src="${profile-photo}"
+                />
+                <h1>${user_name}</h1>
+              </div>
+              <p style="color: white">...</p>
+            </div>
+            <div class="main-text">
+              <p>${tweet-text}
+              </p>
+            </div>
+            <div class="likes">
+              <img
+                class="like-img"
+                src="/Frontend/home-page/Images/heart-like.svg"
+              />
+              <p id="likes-number">${likes_number}</p>
+            </div>
+          </div>`;
