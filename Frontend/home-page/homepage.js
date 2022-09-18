11aa2c64e3e8285ea0b31f91813264461236d793
“Housam <housam.kak20@gmail.com>
2022-09-18 22:20:50 +0300

    Constructing the

--- Frontend/home-page/homepage.js
+++ Frontend/home-page/homepage.js
@@ -1,3 +1,27 @@
 // logged_in_user = localStorage.getItem("active-user")
 //   ? JSON.parse(localStorage.getItem("active-user"))
 //   : "";
+tweet=
+<div class="happening">
+  <h1>Home</h1>
+  <div class="im-happen">
+    <img class="profile-photo" src="/Frontend/home-page/Images/demo-img.jpg" />
+    <input placeholder="What's happening?" />
+  </div>
+  <div class="tweet-btn">
+    <button>Tweet</button>
+  </div>
+</div>;
+
+
+let ahmad = `<div class="happening">
+<h1>Home</h1>
+<div class="im-happen">
+  <img class="profile-photo" src="${}" />
+  <input placeholder="What's happening?" />
+</div>
+<div class="tweet-btn">
+  <button>Tweet</button>
+</div>
+</div>`
+
